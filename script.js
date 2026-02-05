1c49c960bcab9bf25f0099434d63288912657b4f
junaidk8675-code <junaidk8675@gmail.com>
2026-02-05 09:37:04 -0500

    fixed my form and added CSS code to it
    adding new features to my code and to my form

--- script.js
+++ script.js
@@ -1,67 +1,129 @@
-console.log("js console");
+console.log("js started");
 
 var data;
 var grid = document.querySelector(".grid-container");
 
-let directorInput = document.querySelector(".grid-container");
 
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+  console.log("Loaded from localStorage");
+  if (grid) {
+    makeCards();
+  }
+} else {
+  var xhttp = new XMLHttpRequest();
 
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      data = JSON.parse(this.responseText);
+      console.log("Loaded from gameData.json");
 
-var xhttp = new XMLHttpRequest();
+      localStorage.setItem("datalist", JSON.stringify(data));
+      console.log("Saved starter data to localStorage");
 
-xhttp.onreadystatechange = function(){
- if (this.readyState == 4 && this.status == 200){
+      if (grid) {
+        makeCards();
+      }
+    }
+  };
 
-    data = JSON.parse(xhttp.responseText);
-    console.log(data);
+  xhttp.open("GET", "movies.json", true);
+  xhttp.send();
+}
 
-    data.forEach(function(movie) {
+// RENDER CARDS
+function makeCards() {
+  grid.innerHTML = "";
+
+  data.forEach(function (movie) {
     let card = document.createElement("div");
     card.classList.add("card");
 
     let textData =
-    "<div class='movie-title'>"+ movie.title + "</div>" +
-    "<span>"+
-    "Director: " + movie.director + "<br>"+
-    "Runtime: " + movie.runTime + "<br>" + 
-    "Genre: " + movie.genre + "<br>" + 
-    "Status: " + movie.status + "<br>" + 
-    
-    "</span>";
+      "<div class='game-title'>" + movie.title + "</div>" +
+      "<div>Publisher: " + movie.director + "</div>" +
+      "<div>Release Date: " + movie.status + "</div>";
 
     card.innerHTML = textData;
+    grid.appendChild(card);
+  });
 
-    if (movie.imgSrc){
-     card.style.backgroundImage = "url('" + movie.imgSrc + "')";
- }
-    
-   grid.appendChild(card);
+  console.log("cards refreshed");
+}
+
+
+
+
+
+console.log("form script started");
+
+if (localStorage.getItem("datalist")) {
+  data = JSON.parse(localStorage.getItem("datalist"));
+} else {
+  data = [];
+}
+
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title-input");
+var pubInput = document.querySelector("#publisher-input");
+var dateInput = document.querySelector("#release-date-input");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  var newObj = {
+    title: titleInput.value,
+    publisher: pubInput.value,
+    releaseDate: dateInput.value
+  };
+
+  data.push(newObj);
+  localStorage.setItem("datalist", JSON.stringify(data));
+  console.log("Saved new item to localStorage");
+
+  // Only render if grid exists on this page
+  if (document.querySelector(".grid-container")) {
+    makeCards();
+  }
+
+  form.reset();
 });
 
-  } 
-
-};
 
 
 
-Form.addEventListener("submit",function(e){
-e.preventDefault();
-let title = titleInput.value;
-let publisher = devInput.value;
-let releaseDate = releaseDateInput.value;
-let gifSrc = gifInput.value;
-let imgSrc = imgInput.value;
-let newObj = {
-      "id":getNextId(),
-      "title": title,
-      "publisher": publisher,
-      "releaseDate":releaseDate,
-      "imgSrc":imgSrc,
-      "gifSrc":gifSrc };
-  submitData(newObj);
-  form.rset();
-  
-});
 
-xhttp.open("GET", "movies.json", true);
-xhttp.send();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
